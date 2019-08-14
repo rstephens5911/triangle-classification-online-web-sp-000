73b597c80384ed60c8157d84d8a0b08b97d6b579
Ronald Stephens <rstephens@bluemorpho.cc>
2019-08-14 20:55:50 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,17 +1,17 @@
 class Triangle
-  attr_reader :a, :b, :c 
+  attr_reader :a, :b, :c
 
   def initialize(a, b, c)
-    @a = a 
-    @b = b 
-    @c = c 
+    @a = a
+    @b = b
+    @c = c
   end
 
   def kind
     validate_triangle
     if a == b && b == c
       :equilateral
-    elsif a == b || b == c || a == c 
+    elsif a == b || b == c || a == c
       :isosceles
     else
       :scalene
