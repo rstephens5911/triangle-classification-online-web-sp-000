44b797e7f719e170e4aad41f5a2798fe13ddb991
Ronald Stephens <rstephens@bluemorpho.cc>
2019-08-14 20:47:11 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,36 @@
 class Triangle
-  # write code here
+
+  def initialize(side_1, side_2, side_3)
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
+  end
+
+  def kind
+
+    if (@side_1 <= 0 || @side_2 <= 0 || @side_3 <= 0)
+        raise TriangleError
+    end
+    if (@side_1 + @side_2 <= @side_3 || @side_2 + @side_3 <= @side_1 || @side_3 + @side_1 <= @side_2)
+      raise TriangleError
+    end
+    if
+      @side_1 == @side_2 && @side_1 == @side_3 && @side_2 == @side_3
+      :equilateral
+    elsif
+      @side_1 != @side_2 && @side_2 != @side_3 && @side_1 != @side_3
+      :scalene
+    elsif
+      @side_1 == @side_2 && @side_1 != @side_3 || @side_2 == @side_3 && @side_1 != @side_3 || @side_1 == @side_3 && @side_2 != @side_3
+      :isosceles
+    end
+  end
+
+  class TriangleError < StandardError
+    def message
+      "these values are invalid for a triangle!"
+    end
+  end
+
+
 end
